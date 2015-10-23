9e8d9455ee26a039720b1b029391b063728b366e
emersion <contact@emersion.fr>
2015-10-23 14:52:32 +0200

    First working version

--- gnomeconnect.go
+++ gnomeconnect.go
@@ -4,19 +4,60 @@ import (
 	"log"
 	"github.com/emersion/go-kdeconnect/engine"
 	"github.com/emersion/go-kdeconnect/plugin"
+	"github.com/emersion/go-kdeconnect/network"
+	"github.com/godbus/dbus"
+	"github.com/esiqveland/notify"
 )
+
+func getDeviceIcon(device *network.Device) string {
+	switch device.Type {
+	case "phone":
+		return "phone"
+	case "tablet":
+		return "pda" // TODO: find something better
+	case "desktop":
+		return "computer"
+	default:
+		return ""
+	}
+}
+
+func newNotification() notify.Notification {
+	return notify.Notification{
+		AppName: "GNOMEConnect",
+	}
+}
 
 func main() {
 	battery := plugin.NewBattery()
 	ping := plugin.NewPing()
 
 	go (func() {
+		conn, err := dbus.SessionBus()
+		if err != nil {
+			panic(err)
+		}
+
+		notifier := notify.New(conn)
+
 		for {
 			select {
 			case event := <-ping.Incoming:
 				log.Println("New ping from device:", event.Device.Name)
+
+				n := newNotification()
+				n.AppIcon = getDeviceIcon(event.Device)
+				n.Summary = "Ping from "+event.Device.Name
+				notifier.SendNotification(n)
 			case event := <-battery.Incoming:
 				log.Println("Battery:", event.Device.Name, event.BatteryBody)
+
+				if event.ThresholdEvent == plugin.BatteryThresholdEventLow {
+					n := newNotification()
+					n.AppIcon = "battery-caution"
+					n.Summary = event.Device.Name+" has low battery"
+					notifier.SendNotification(n)
+				}
 			}
 		}
 	})()
